b36c90d07e3f5d9307484b31359ac68240e0715a
ahmadnurfauziii <1941720068@student.polinema.ac.id>
2021-12-27 17:44:13 +0700

    Test Change Password

---
+++ cypress/integration/(3) Ahmad Nur Fauzi/changepassword4.spec.js
@@ -0,0 +1,29 @@
+describe('Login', function (){
+    beforeEach(() => {        
+        cy.visit('http://lms.polinema.ac.id/login/index.php')
+        // Halaman Login
+        cy.get('#username').type('1941720068')
+        // Input Username
+        cy.get('#password').type('Fauzi12345')
+        // Input Password                
+        cy.get('[class="btn btn-primary btn-block m-t-1"]').should('be.visible').click()   
+        // Button Login
+    })
+    it('Change Password Dengan Numerik dan Special Character', () => {  
+        cy.visit('http://lms.polinema.ac.id/user/preferences.php')
+        // Masuk Halaman Pengaturan
+        cy.visit('http://lms.polinema.ac.id/login/change_password.php?id=1')
+        // Masuk Halaman Change Password
+        cy.get('#id_password').type('Fauzi12345')
+        // Input Current Password
+        cy.get('#id_newpassword1').type('1941720068_')
+        // Input New Password 1
+        cy.get('#id_newpassword2').type('1941720068_')
+        // Input New Password 2
+        cy.get('#id_submitbutton').should('be.visible').click()
+        // Button Submit 
+        cy.get('[class="btn btn-primary"]').should('be.visible').click()   
+        // Button Countineu
+    })
+})    
+    

89aebb8757e7554992241739fad80d05f5243ea6
= <=>
2022-01-03 00:52:18 +0700

    Test Case Grade

---
+++ cypress/integration/(7) Aulia Rahman/testcaseGrade5.js
@@ -0,0 +1,18 @@
+describe('Login', function (){
+    beforeEach(() => {        
+        cy.visit('http://lms.polinema.ac.id/login/index.php')
+        //halaman Login
+        cy.get('#username').type('1941720209')
+        //input Username
+        cy.get('#password').type('nooname12')
+        //input Password
+        cy.get('[class="btn btn-primary btn-block m-t-1"]').should('be.visible').click()
+        //button Login
+    })
+    it('Grade per assignment', () =>{
+        cy.visit('http://lms.polinema.ac.id/course/view.php?id=715')
+        //halaman Algoritma Struktur data
+        cy.visit('http://lms.polinema.ac.id/mod/assign/view.php?id=10166')
+        //halaman Nilai assignment
+    })
+})
